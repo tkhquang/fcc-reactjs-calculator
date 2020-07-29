21c8d58ff72a34b2a7e9652c79e274227fcd9949
Quang Trinh <khacquang.trinh@gmail.com>
2020-07-30 00:45:24 +0700

    Refactor

---
+++ docs/precache-manifest.e846b2d813deb7fcf344e5a5e07d6b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4c86562d0e809ec54d2e108b4016af",
+    "url": "/simple-reactjs-calculator/index.html"
+  },
+  {
+    "revision": "acf46dbf8c7caf447e14",
+    "url": "/simple-reactjs-calculator/static/css/main.f29cd973.chunk.css"
+  },
+  {
+    "revision": "66f51e9dc53be5cf37f0",
+    "url": "/simple-reactjs-calculator/static/js/2.83e6b2b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-reactjs-calculator/static/js/2.83e6b2b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf46dbf8c7caf447e14",
+    "url": "/simple-reactjs-calculator/static/js/main.385f63d7.chunk.js"
+  },
+  {
+    "revision": "baeba59ee23046f4ff2e",
+    "url": "/simple-reactjs-calculator/static/js/runtime-main.c978e253.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6bb271530485b9656bd8a819e3746c6.js"
+  "/simple-reactjs-calculator/precache-manifest.e846b2d813deb7fcf344e5a5e07d6b5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-reactjs-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
